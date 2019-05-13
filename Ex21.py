1672d626c5faff262bed2ec03c64b4b7a45b3ced
Faith Pham <student.138059@worcesterschools.net>
2019-05-13 13:18:56 +0000

    second commit

--- Ex21.py
+++ Ex21.py
@@ -1,32 +1,33 @@
+#adds the A and B variables together
 def add(a, b):
    print(f"ADDING {a} + {b}")
    return a + b
-
+#subtracts the A and B variable together 
 def subtract(a, b):
     print(f"subtracting {a} - {b}")
     return a - b
-    
+#multiplys the A and B variable together    
 def multiply(a, b):
     print(f"MULTIPLYING {a} + {b}")
     return a * b
-    
+#Divides the A and B variable together
 def divide(a, b):
     print(f"DIVIDE {a} / {b}")
     return a / b
     
 
 print("Let's do some math with just funtions!")
-
+#solves math problems using the A,B formal from before for age, height, weight, and IQ
 age = add(10, 4)
 height = subtract(63,2)#inches
 weight = multiply(70,2)#lbs
-iq = divide(100,2)#this is just the average iq
-
+iq = divide(200,2)#this is just the average iq
+#the answers
 print(f"Age: {age}, Height:{height}, Weight: {weight}, IQ: {iq}")
 
 
 print("Here is a puzzle.")
-
+#math problems :P
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print("That becomes: ", what,"Can you do it by hand?")
---
+++ Ex22.py
@@ -0,0 +1,41 @@
+print()#prints the given function 
+""#
+()#
+{}#
+print(f"")#
+#Everything after the octothrope python will ignore allowing the programmer make comments
++#
+=#
+-#
+/#
+%#represent modulus can also used for formatting a string
+print("""   """)#Prints the given funtions but the """ allows it to be used on mutiple lines until closed
+n/#Escape sequences
+\t#Escape sequences
+\\#Escape sequences
+end=' ')#
+from#
+sys#
+import#
+argv#
+input()#
+open()#
+exit#
+def#
+(*argv)#
+return# returns a value to its caller
+if # let you make decisons in your python code
+\'   #Escape sequences
+\"   #Escape sequences 
+\a#Escape sequences
+\b#Escape sequences
+\f#Escape sequences
+\n#Escape sequences
+\N{name}#Escape sequences
+\r #Escape sequences
+\t#Escape sequences
+\uxxxx#Escape sequences
+\Uxxxxxxxx#Escape sequences
+\v#Escape sequences
+\ooo#Escape sequences
+\xhh#Escape sequences
